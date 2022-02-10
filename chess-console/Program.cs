82f6e430c1024a57d3d525bc5b0ac1098bb486a3
marcos <marcosviniciocardoso123@gmail.com>
2022-02-10 19:42:57 -0300

    feature -- add classe posicao

--- chess-console/Program.cs
+++ chess-console/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using chess_console.tabuleiro;
+
+namespace chess_console
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Posicao p1 = new Posicao(3, 4);
+            Console.WriteLine("Posição: " + p1);
+        }
+    }
+}
