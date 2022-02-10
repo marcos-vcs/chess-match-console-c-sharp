ed2c68615824be677dd3f9e293ab4e4a2685bf47
marcos <marcosviniciocardoso123@gmail.com>
2022-02-10 20:40:14 -0300

    improvement -- tratamento de erros com exceção personalizada

--- chess-console/tabuleiro/Tabuleiro.cs
+++ chess-console/tabuleiro/Tabuleiro.cs
@@ -24,12 +24,45 @@ namespace chess_console.tabuleiro
             return this.Pecas[linha, coluna];
         }
 
+        public Peca Peca(Posicao pos)
+        {
+            return this.Pecas[pos.Linha, pos.Coluna];
+        }
+
+        public bool ExistePeca(Posicao pos)
+        {
+            ValidarPosicao(pos);
+            return Peca(pos) != null;
+        
+        }
+
         public void ColocarPeca(Peca p, Posicao pos) 
         {
+            if (ExistePeca(pos))
+            {
+                throw new TabuleiroException("Ja existe uma peca nessa posicao");
+            }
+
             Pecas[pos.Linha, pos.Coluna] = p;
             p.Posicao = pos;
         }
 
+        public bool PosicaoValida(Posicao pos)
+        {
+            if (pos.Linha < 0 || pos.Linha >= Linhas || pos.Coluna < 0 || pos.Coluna >= Colunas)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void ValidarPosicao(Posicao posicao)
+        {
+            if (!PosicaoValida(posicao))
+            {
+                throw new TabuleiroException("Posicao invalida!");
+            }
+        }
 
 
     }
