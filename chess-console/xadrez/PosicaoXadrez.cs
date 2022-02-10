8b0dd8f0d8a889ef0c528f91fd91d96d0d6f9fec
marcos <marcosviniciocardoso123@gmail.com>
2022-02-10 20:55:52 -0300

    feature -- add classe PosicaoXadrez

---
+++ chess-console/xadrez/PosicaoXadrez.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chess_console.xadrez
+{
+    class PosicaoXadrez
+    {
+        public char Coluna { get; set; }
+        public int Linha { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            Coluna = coluna;
+            Linha = linha;
+        }
+
+        public override string ToString()
+        {
+            return "" + Coluna + Linha;
+        }
+
+    }
+}
